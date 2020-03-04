0574b314b83d05688c726afa3c13a6a9364774eb
Yaroslav Vishnevsky <rick.ua@gmail.com>
2020-03-04 17:54:56 +0200

    refactoring indexing

---
+++ src/Erc.Households.Indexing/IndexongService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Erc.Households.Indexing
+{
+    public class IndexongService :IHostedService
+    {
+        private readonly ILogger<IndexongService> _logger;
+        private readonly IBusControl _busControl;
+
+        public IndexongService(ILogger<IndexongService> logger, IBusControl busControl)
+        {
+            _logger = logger;
+            _busControl = busControl;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _busControl.StopAsync(cancellationToken);
+            _logger.LogInformation("Indexing service stopped");
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _busControl.StartAsync();
+            _logger.LogInformation("Indexing service started");
+        }
+    }
+}
--- src/Erc.Households.Indexing/Program.cs
+++ src/Erc.Households.Indexing/Program.cs
@@ -2,8 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Erc.Households.Indexing.EventHandlers;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Nest;
 
 namespace Erc.Households.Indexing
 {
@@ -16,9 +21,26 @@ namespace Erc.Households.Indexing
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddEnvironmentVariables().AddJsonFile("appsettings.json");
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<IElasticClient>(provider => new ElasticClient(new Uri(hostContext.Configuration.GetConnectionString("Elasticsearch"))));
+
+                services.AddMassTransit(x =>
                 {
-                    services.AddHostedService<Worker>();
+                    x.AddConsumer<AccountingPointPersistedEventHandler>();
+
+                    x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                    {
+                        cfg.Host(hostContext.Configuration.GetConnectionString("RabbitMQ"));
+                        cfg.ConfigureEndpoints(provider);
+                    }));
+                        
+                    services.AddHostedService<IndexongService>();
                 });
+            });
     }
 }
