b77c17d996f8ddb9b909fe848e06c41cffdb8ca1
max <ztgordey@gmail.com>
2020-10-30 10:42:14 +0200

    Update BranchOffice model

--- src/Erc.Households.Server.Domain/BranchOffice.cs
+++ src/Erc.Households.Server.Domain/BranchOffice.cs
@@ -13,8 +13,9 @@ namespace Erc.Households.Domain
         public string Address { get; private set; }
         public Commodity[] AvailableCommodities { get; set; }
         public int CurrentPeriodId { get; private set; }
-        public int? CompanyId { get; set; }
-        public string AccountNumber { get; set; }
+        public int CompanyId { get; set; }
+        public string Iban { get; set; }
+        public string BankFullName { get; set; }
         public string ChiefName { get; set; }
         public string BookkeeperName { get; set; }
         public Period CurrentPeriod { get; private set; }
