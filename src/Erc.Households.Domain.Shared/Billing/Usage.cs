c9e2ce26e33b74668c54428ef2be1a983c322350
Yaroslav <rick@ztoe.com.ua>
2021-05-25 11:27:52 +0300

    update packages

--- src/Erc.Households.Domain.Shared/Billing/Usage.cs
+++ src/Erc.Households.Domain.Shared/Billing/Usage.cs
@@ -5,11 +5,18 @@ namespace Erc.Households.Domain.Shared.Billing
 {
     public class Usage
     {
-        public Usage(decimal units, decimal kz = 1, decimal discountWeight = 1)
+        //protected Usage(decimal units, decimal charge, decimal kz, decimal discountWeight)
+        //    :this(units, kz, discountWeight)
+        //{
+        //    Charge = charge;
+        //}
+
+        public Usage(decimal units, decimal kz = 1, decimal discountWeight = 1, decimal charge = 0)
         {
             Units = units;
             Kz = kz;
             DiscountWeight = discountWeight;
+            Charge = charge;
         }
 
         public int? PresentMeterReading { get; init; }
